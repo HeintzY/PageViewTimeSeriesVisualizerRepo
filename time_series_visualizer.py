e4b39a7f5367eea52da95b7de421e6ce17aeb4de
HeintzY a <jschoeniger@gmx.de>
2024-12-05 08:30:20 +0000

    feat: Add finished project

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,15 +5,21 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=['date']).set_index('date')
+
 
 # Clean data
-df = None
+df = df[(df['value'] <= df['value'].quantile(0.975)) & (df['value'] >= df['value'].quantile(0.025))]
 
 
 def draw_line_plot():
     # Draw line plot
-
+    fig, ax = plt.subplots(figsize=(20, 5))
+    plt.plot(df, color='red')
+    plt.xlabel('Date')
+    plt.ylabel('Page Views')
+    plt.title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    
 
 
 
@@ -24,17 +30,22 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    
+    df_bar = df.groupby([df.index.year, df.index.month]).mean()
+
 
     # Draw bar plot
 
-
+    fig = df_bar.unstack().plot.bar(figsize=(9, 9), ylabel='Average Page Views', xlabel='Years').figure
+    
+    plt.legend(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'], title='Months')
 
 
 
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
     return fig
+
 
 def draw_box_plot():
     # Prepare data for box plots (this part is done!)
@@ -44,7 +55,19 @@ def draw_box_plot():
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
     # Draw box plots (using Seaborn)
+    df_box['month_number'] = df_box['date'].dt.month
+    df_box = df_box.sort_values('month_number')
+    del df_box['month_number']
 
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(20,10))
+    ax[0] = sns.boxplot(df_box, x='year', y='value', ax=ax[0], palette='rainbow')
+    ax[0].set_xlabel('Year')
+    ax[0].set_ylabel('Page Views')
+    ax[0].set_title('Year-wise Box Plot (Trend)')
+    ax[1] = sns.boxplot(df_box, x='month', y='value', ax=ax[1], palette='rainbow')
+    ax[1].set_xlabel('Month')
+    ax[1].set_ylabel('Page Views')
+    ax[1].set_title('Month-wise Box Plot (Seasonality)')
 
 
 
